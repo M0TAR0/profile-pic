d2475d6904deedf38fe558808a06462db4a7039c
Romel <romel.ontiv@gmail.com>
2026-08-02 16:26:21 -0600

    Added Watchdog and threading tests

--- Watchdog/watchdog_test.py
+++ Watchdog/watchdog_test.py
@@ -1,1 +1,31 @@
+import time
+from watchdog import events
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+
+class myHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print("You just created:", event.src_path)
+
+
+creation_handler = myHandler()
+creation_observer = Observer()
+creation_observer.schedule(
+    creation_handler, path=os.getenv("TEST_DIRECTORY"), recursive=False
+)  # type: ignore
+creation_observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    creation_observer.stop()
+
+creation_observer.join()
+
+print("\nYOU FINISHED THE OBSERVATION!")
